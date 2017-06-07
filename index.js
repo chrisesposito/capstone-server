2717946feed42fed7ebcbc6252e7283fc1ae5aae
chris esposito <chris.esposito@gmail.com>
2017-06-06 21:22:14 -0700

    added mysql connect code - it works

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express();
+var mysql = require('mysql');
+var connection;
+
 
 app.get('/records/:datewanted', function(req, resp) {
     var dateWanted = req.params.datewanted;
@@ -14,5 +17,26 @@ app.get('/records/:datewanted', function(req, resp) {
   }
 );
 
+
+connection = mysql.createConnection(
+                {
+                    host: 'localhost',
+                    database: 'testData',
+                    user: 'root',
+                    password: 'Arisb@d1mss1'
+
+                }
+        );
+        connection.connect(function (err)
+        {
+            if (err)
+            {
+                console.error('error connection to DB: ' + err.stack);
+                return;
+            }
+            console.log('connected to db as as id ' + connection.threadId);
+        }
+        );
 app.listen(3001);
 console.log("server listening on port 3001");
+
